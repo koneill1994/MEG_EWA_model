c659515bd6452855e8f07b62148c8ba670c39f3b
koneill1994 <koneill1994@gmail.com>
2019-02-10 14:45:05 -0500

    Updating model

--- EWA_model.R
+++ EWA_model.R
@@ -99,12 +99,14 @@ psd=c(.01,.01,.01,.01)
 # initial choice probabilities based on human data
 choice_prob_data=c(0.025, 0.100, 0.200, 0.250, 0.175, 0.075, 0.175)
 
+# number of simulations to run
+n_sims=100
 
-model_data=data.frame()
+model_data_full=data.frame()
 
 if(TRUE){
   # gotta debug other parts first
-  for(sim in 1:100){
+  for(sim in 1:n_sims){
     print(paste("sim ",sim))
     
     model_list=c(Make_EWA_model("1",p_m,psd,choice_prob_data),
@@ -126,9 +128,68 @@ if(TRUE){
         # log data
         header=data.frame(sim=sim, model=model$id, round=round)
         m_log=cbind(header,model$output_logfile())
-        model_data=rbind(model_data, m_log)
+        model_data_full=rbind(model_data_full, m_log)
       }
     }
   }
 
 }
+
+
+
+
+# now we read in the actual data
+
+source("Data_Analysis.R")
+# this should give us JUST d1a as a dataframe object
+
+# game, subject, round, choice
+
+human_data=data.frame(group=d1a$Group, subject=d1a$Subject_ID, round=d1a$Round, choice=d1a$Effort_level)
+model_data=model_data_full[,1:4]
+# lets check correlation & rmse between corresponding average choice in rounds
+# for human and model
+
+
+# average across groups and players
+# to get average choice made for each round for humans and models
+compare_dat=data.frame(unique(select(mutate(group_by(model_data, round),
+                                            model_average=mean(own_choice),
+                                            model_se=sd(own_choice)/sqrt(length(own_choice))
+                                            ),
+                                     round,model_average,model_se)),
+                       unique(select(mutate(group_by(human_data, round),
+                                            human_average=mean(choice),
+                                            human_se=sd(choice)/sqrt(length(choice))
+                                            ),
+                                     round, human_average, human_se))
+)[,c("round", "model_average", "model_se", "human_average", "human_se")]
+# there might be a better way to do the above so that ggplot lets me 
+# treat a human_or_model variable as a grouping variable and gives me a legend
+
+
+
+# look at it visually
+ggplot(compare_dat)+
+  geom_line(aes(x=round, y=human_average), color="red")+
+  geom_line(aes(x=round, y=model_average))+
+  geom_errorbar(mapping=aes(x=round, ymin=human_average-human_se, ymax=human_average+human_se), 
+                color="red", width=.2)+
+  geom_errorbar(mapping=aes(x=round, ymin=model_average-model_se, ymax=model_average+model_se),
+                width=.2)+
+  labs("average choice")+
+  labs(title="average choice by round for human and model agents",
+       subtitle="error bars represent standard error",
+       caption="human data is in red, model data is in black")
+
+# correlation
+cor(compare_dat$model_average, compare_dat$human_average)
+
+# rmse
+# sqrt(mean((m - o)^2))
+sqrt(mean((compare_dat$model_average-compare_dat$human_average)^2))
+
+
+
+
+# for running simulations: run 10 or so at each unique parameter values
